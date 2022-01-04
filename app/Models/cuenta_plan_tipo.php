780f52e4f573315c74d380b9c6093d4fde34b3fe
vilagutmercy <88634329+vilagutmercy@users.noreply.github.com>
2022-01-03 23:47:46 -0400

    baldiviesowilberfernando
    migraciones

---
+++ app/Models/cuenta_plan_tipo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class cuenta_plan_tipo extends Model
+{
+    use HasFactory;
+}
